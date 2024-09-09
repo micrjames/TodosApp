3d1c88f494656532ebf8629e72ab697cace26e37
Michael James <micrjames921@gmail.com>
2024-09-09 14:37:33 -0700

    refactored 'l_s' utils controller code; created directory for 'users' file

--- src/controllers/utils/l_s.ts
+++ src/controllers/utils/l_s.ts
@@ -1,6 +1,4 @@
 import { Response } from "express";   
-import path from "path";
-import { utils } from "../../utils.js";
 import { forms } from "../../data/forms/forms.js";
 import { send } from "./send.js";
 
@@ -8,12 +6,16 @@ export const sendLS = (res: Response, status: number, msgs: {
 														 err_msg: string;
 														 required_msg: string;	
 													  }) => {
-   res.status(status).render(path.resolve(utils.__dirname, "./public/views/l_s.ejs"), {
+   send(res, { 
+	  status,
+	  view: "./public/views/l_s.ejs",
+	  data: {
 		 err_msg: msgs.err_msg,
 		 header: "Login/Signup Below",
 		 login: "Login",
 		 signup: "Signup",
 		 forms,
 		 required_msg: msgs.required_msg
-   });
+	  }
+   });														 
 };
